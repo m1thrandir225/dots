669a595e3697e0ed5c8a18ca9d2ec9c96f9fa055
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-17 10:45:43 +0100

    fix: revert neovim config to lazy

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,54 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+--
+--
+vim.opt.wrap = true
+vim.opt.textwidth = 80
+vim.g.lazyvim_php_lsp = "intelephense"
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/typescript.nvim",
+      init = function()
+        require("lazyvim.util").lsp.on_attach(function(_, buffer)
+          -- stylua: ignore
+          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+        end)
+      end,
+    },
+    {
+      -- Add a Treesitter parser for Laravel Blade to provide Blade syntax highlighting.
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        vim.list_extend(opts.ensure_installed, {
+          "blade",
+          "php_only",
+        })
+      end,
+      config = function(_, opts)
+        vim.filetype.add({
+          pattern = {
+            [".*%.blade%.php"] = "blade",
+          },
+        })
+
+        require("nvim-treesitter.configs").setup(opts)
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        parser_config.blade = {
+          install_info = {
+            url = "https://github.com/EmranMR/tree-sitter-blade",
+            files = { "src/parser.c" },
+            branch = "main",
+          },
+          filetype = "blade",
+        }
+      end,
+    },
+  },
+  -- use mini.starter instead of alpha
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+}
