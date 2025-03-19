2a952a1071f61a6dc613dfde9755bc3839caf2a5
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-19 10:49:20 +0100

    feat: tailwindv4 lsp & color settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -51,4 +51,23 @@ return {
   },
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
+  -- cmp tailwind highlighting
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "tailwind-tools",
+      "onsails/lspkind-nvim",
+      -- ...
+    },
+    opts = function()
+      return {
+        -- ...
+        formatting = {
+          format = require("lspkind").cmp_format({
+            before = require("tailwind-tools.cmp").lspkind_format,
+          }),
+        },
+      }
+    end,
+  },
 }
