73be98597f4c4304f8897f8c1cd1bbe0fb1668d0
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-03-24 17:58:26 +0100

    fix: use the default formatter for php

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -1,14 +1,5 @@
 return {
   {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        php = { { "pint", "php_cs_fixer" } },
-      },
-    },
-  },
-  {
     -- Remove phpcs linter.
     "mfussenegger/nvim-lint",
     optional = true,
