4c56bab26209562e6a0dfeede68444511f01f050
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-04-07 10:54:47 +0200

    feat: tmux-sessionizer adjustments
    the sessionizer opens two new windows: one with neovim and one empty
    one, the default one selected is the neovim one

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -14,7 +14,14 @@ selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s $selected_name -c $selected
+  # Open the selected directory with neovim open and an empty window
+  tmux new-session -s $selected_name -c $selected -n "nvim" -d
+
+  tmux new-window -t $selected_name -c $selected -n "shell"
+
+  tmux send-keys -t $selected_name:0 "nvim ." C-m
+
+  tmux select-window -t $selected_name:0
   exit 0
 fi
 
