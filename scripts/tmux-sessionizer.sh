82800de846fcfae89280b048e7c1b1ad5f73be74
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-04-13 10:48:42 +0200

    feat: tmux nvim and scripts modifications

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
+switch_to() {
+  if [[ -z $TMUX ]]; then
+    tmux attach-session -t $1
+  else
+    tmux switch-client -t $1
+  fi
+}
+
+has_session() {
+  tmux list-sessions | grep -q "^$1:"
+}
+
+hydrate() {
+  if [ -f $2/.tmux-sessionizer ]; then
+    tmux send-keys -t $1 "source $2/.tmux-sessionizer" c-M
+  elif [ -f $HOME/.tmux-sessionizer ]; then
+    tmux send-keys -t $1 "source $HOME/.tmux-sessionizer" c-M
+  fi
+}
 
 if [[ $# -eq 1 ]]; then
   selected=$1
 else
-  selected=$(find ~/Documents/Projects ~/Documents/Skolo/ -mindepth 1 -maxdepth 1 -type d | fzf)
+  selected=$(find ~/.config ~/Documents/Projects ~/Documents/Projects/Skolo -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
@@ -14,23 +33,14 @@ selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  # Open the selected directory with neovim open and an empty window
-  tmux new-session -s $selected_name -c $selected -n "nvim" -d
-
-  tmux new-window -t $selected_name -c $selected -n "shell"
-
-  tmux send-keys -t $selected_name:0 "nvim ." C-m
-
-  tmux select-window -t $selected_name:0
+  tmux new-session -s $selected_name -c $selected
+  hydrate $selected_name $selected
   exit 0
 fi
 
-if ! tmux has-session -t=$selected_name 2>/dev/null; then
+if ! has_session $selected_name; then
   tmux new-session -ds $selected_name -c $selected
+  hydrate $selected_name $selected
 fi
 
-if [[ -z $TMUX ]]; then
-  tmux attach -t $selected_name
-else
-  tmux switch-client -t $selected_name
-fi
+switch_to $selected_name
