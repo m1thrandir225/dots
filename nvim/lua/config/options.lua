983de0a01aeeb6ade107194f02f733a966b10a45
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-12-24 21:17:33 +0100

    feat: python-uv added

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,7 +9,7 @@ vim.opt.textwidth = 80
 vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.lazyvim_blink_main = false
 
-vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_prettier_needs_config = true
 
 vim.filetype.add({
